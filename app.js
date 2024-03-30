20af8fb6ccf58e2110a0bfdd1b6a4a253f046b7b
lalitanar <lalita.nar@mahidol.edu>
2024-03-30 20:34:14 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => res.send('Hello World Here are the update version'))
 
 app.listen(3000, () => console.log('server start'))
-//update
+
